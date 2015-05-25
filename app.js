6fcdf23687dcba40187664e684d94ef0d7c5c64b
Jorge Guitard T <jguitard@remoteando.cl>
2015-05-25 20:37:31 -0300

    Correcciones varias

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 var routes = require('./routes/index');
 var api = require('./routes/api');
@@ -19,6 +20,7 @@ app.set(
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(methodOverride('_method'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
